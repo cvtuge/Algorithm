f578e34d707f94acbbce894ec81ef577e74b0aa6
cvtuge <daomujun@foxmail.com>
2019-10-22 09:45:28 -0700

    commit 8queen first version

---
+++ 8queen/8queen.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+static int result[8]={0};
+
+bool is_ok(int col, int raw)
+{
+    int leftup=col-1;
+    int rightup = col+1;
+    for(int 0)
+}
+
+void main()
+{
+}
+
